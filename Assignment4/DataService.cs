04b361269e67a87cdc93de6dd730b2023964b56b
jakshob <jakshob@hotmail.com>
2018-10-16 15:18:44 +0200

    gammel

--- Assignment4/DataService.cs
+++ Assignment4/DataService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Assignment4;
+using Remotion.Linq.Utilities;
 
 namespace Assignment4
 {
@@ -17,5 +18,63 @@ namespace Assignment4
             }
         }
 
+        public Category GetCategory(int index)
+        {
+
+            using (var db = new NorthwindContex())
+            {
+                return db.Categories.Find(index);
+            }
+
+        }
+
+        public Category CreateCategory(string name, string description)
+        {
+            using (var db = new NorthwindContex())
+            {
+                var cat = new Category();
+                List<Category> tempList = db.Categories.ToList<Category>();
+
+                var item = tempList[tempList.Count - 1];
+
+
+                db.Categories.Add(new Category()
+                {
+                    Id = item.Id + 1,
+                    Name = name,
+                    Description = description
+                });
+
+                db.SaveChanges();
+
+                foreach (var category in db.Categories)
+                {
+                    if (category.Name == name) return category;
+                }
+
+                return cat;
+
+            }
+        }
+        
+        public bool DeleteCategory(int i)
+        {
+            using (var db = new NorthwindContex())
+            {
+                foreach (var elem in db.Categories)
+                {
+                    if (elem.Id == i)
+                    {
+                        db.Categories.Remove(elem);
+                        db.SaveChanges();
+                        return true;
+                    }
+                    else return false;
+                }
+
+                return false;
+
+            }
+        }
     }
 }
